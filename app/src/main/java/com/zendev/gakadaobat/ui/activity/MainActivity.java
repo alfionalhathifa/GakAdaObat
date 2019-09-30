17107fdc38be3c94850478b3dba8b21be3c0f3b1
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-10-01 00:17:42 +0700

    Add FoodFragment and FavoriteFragment

--- app/src/main/java/com/zendev/gakadaobat/ui/activity/MainActivity.java
+++ app/src/main/java/com/zendev/gakadaobat/ui/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.zendev.gakadaobat;
+package com.zendev.gakadaobat.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.zendev.gakadaobat.R;
 
 public class MainActivity extends AppCompatActivity {
 
