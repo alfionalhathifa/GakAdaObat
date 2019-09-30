bbe85676e887f2a6b23b6e367117b41beb335a87
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-10-01 00:38:31 +0700

    Update Bottom Menu and MainActivity

--- app/src/main/java/com/zendev/gakadaobat/ui/activity/MainActivity.java
+++ app/src/main/java/com/zendev/gakadaobat/ui/activity/MainActivity.java
@@ -1,16 +1,53 @@
 package com.zendev.gakadaobat.ui.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.zendev.gakadaobat.R;
+import com.zendev.gakadaobat.ui.fragment.FavoriteFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+
+            switch (item.getItemId()) {
+                case R.id.navigation_list:
+                    fragment = new ListFragment();
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container_layout, fragment, fragment.getClass().getSimpleName())
+                            .commit();
+                    return true;
+                case R.id.navigation_favorite:
+                    fragment = new FavoriteFragment();
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container_layout, fragment, fragment.getClass().getSimpleName())
+                            .commit();
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigationView = findViewById(R.id.navigation);
+        navigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        if (savedInstanceState == null) {
+            navigationView.setSelectedItemId(R.id.navigation_list);
+        }
     }
 }
